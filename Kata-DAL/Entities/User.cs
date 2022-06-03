dd93e79d0e557cf8ad0d830c34fd57eabff69f14
Michal <v25@vault25.net>
2022-06-03 17:24:05 +0200

    added DAL - Entities

---
+++ Kata-DAL/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Kata_DAL.Entities;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string DisplayName { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public List<Message> Timeline { get; set; }
+}
