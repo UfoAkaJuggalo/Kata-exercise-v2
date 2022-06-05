e8fdcd615fdff7929fce5185d15b8e67551008d5
Michal <v25@vault25.net>
2022-06-05 18:18:46 +0200

    added query to get all messages from a user timeline

---
+++ Kata-Services/Queries/GetAllMessagesByUserQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Kata_DAL.Entities;
+using Kata_DAL.IRepositories;
+using MediatR;
+
+namespace Kata_Services.Queries;
+
+public class GetAllMessagesByUserQueryHandler : IRequestHandler<GetAllMessagesByUserQuery, IEnumerable<GetAllMessagesByUserViewModel>>
+{
+    private readonly IMessageRepository _messageRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllMessagesByUserQueryHandler(IMessageRepository messageRepository, IMapper mapper)
+    {
+        _messageRepository = messageRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<GetAllMessagesByUserViewModel>> Handle(GetAllMessagesByUserQuery request, CancellationToken cancellationToken) =>
+        _messageRepository.GetAllMessagesByUserId(request.UserId)
+            .Select(message => _mapper.Map<GetAllMessagesByUserViewModel>(message)).ToList();
+}
---
+++ Kata-Services/Queries/GetAllMessagesByUserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Kata_Services.Queries;
+
+public class GetAllMessagesByUserViewModel
+{
+    public int MessageId { get; set; }
+    public string Content { get; set; }
+    public DateTime DateTime { get; set; }
+}
