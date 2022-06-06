a65f2ea3a18243d7b047e13e770e43930c80a0d5
Michal <v25@vault25.net>
2022-06-06 09:44:22 +0200

    added GetFeedQuery

---
+++ Kata-Services/Queries/GetFeed/GetFeedViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Kata_Services.CommonViewModels;
+
+namespace Kata_Services.Queries.GetFeed;
+
+public class GetFeedViewModel
+{
+    public int MessageId { get; set; }
+    public string Content { get; set; }
+    public DateTime DateTime { get; set; }
+    public UserInfoViewModel Author { get; set; }
+}
