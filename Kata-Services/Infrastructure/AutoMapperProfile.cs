89c2521a39fe17f6c3c6bb31be4840f4a2379e8d
Michal <v25@vault25.net>
2022-06-06 09:44:03 +0200

    added UserInfoViewModel and define mappings in automapper

--- Kata-Services/Infrastructure/AutoMapperProfile.cs
+++ Kata-Services/Infrastructure/AutoMapperProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Kata_DAL.Entities;
+using Kata_Services.CommonViewModels;
 using Kata_Services.Queries;
+using Kata_Services.Queries.GetFeed;
 
 namespace Kata_Services.Infrastructure;
 
@@ -9,5 +11,7 @@ public class AutoMapperProfile : Profile
     public AutoMapperProfile()
     {
         CreateMap<Message, GetAllMessagesByUserViewModel>();
+        CreateMap<Message, GetFeedViewModel>();
+        CreateMap<User, UserInfoViewModel>();
     }
 }
