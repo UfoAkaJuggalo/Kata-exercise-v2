bce5c6a25130a1ead720f318bb6810748d9df3e8
Michal <v25@vault25.net>
2022-06-10 16:55:54 +0200

    Services - small update of AddMessageV3ToTimelineViewModel

--- Kata-Services/Commands/AddMessageV3ToTimeline/AddMessageV3ToTimelineViewModel.cs
+++ Kata-Services/Commands/AddMessageV3ToTimeline/AddMessageV3ToTimelineViewModel.cs
@@ -7,5 +7,5 @@ public class AddMessageV3ToTimelineViewModel
     public string Content { get; set; }
     public int AuthorId { get; set; }
     public IEnumerable<UserInfoMentionsViewModel>? Mentions { get; set; }
-    public IEnumerable<LinksViewModel> Links { get; set; }
+    public IEnumerable<LinksViewModel>? Links { get; set; }
 }
