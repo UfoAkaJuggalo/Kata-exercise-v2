7b083a0b5539d4def867aa7af36d63e8734973fc
Michal <v25@vault25.net>
2022-06-10 12:29:52 +0200

    Services - added LinksViewModel

---
+++ Kata-Services/CommonViewModels/LinksViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Kata_Services.CommonViewModels;
+
+public class LinksViewModel
+{
+    public int LinkId { get; set; }
+    public string Description { get; set; }
+    public string LinkAdress { get; set; }
+}
