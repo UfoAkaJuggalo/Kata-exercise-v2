a31e1070c968272cf7cd4fec5d90fceb2288acf8
Michal <v25@vault25.net>
2022-06-10 16:56:55 +0200

    services - added AddMessageV3ToTimelineCommandHandler

---
+++ Kata-Services/Commands/AddMessageV3ToTimeline/AddMessageV3ToTimelineCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Kata_DAL.IRepositories;
+using MediatR;
+
+namespace Kata_Services.Commands.AddMessageV3ToTimeline;
+
+public class AddMessageV3ToTimelineCommandHandler : IRequestHandler<AddMessageV3ToTimelineCommand, int>
+{
+    private readonly IMessageRepository _messageRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public AddMessageV3ToTimelineCommandHandler(IMessageRepository messageRepository, IMapper mapper, IUserRepository userRepository)
+    {
+        _messageRepository = messageRepository;
+        _mapper = mapper;
+        _userRepository = userRepository;
+    }
+
+    public Task<int> Handle(AddMessageV3ToTimelineCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
